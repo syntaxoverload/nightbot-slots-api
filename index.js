e074bbb2ba213e02ffc862405166268d67b6712c
Steven S <stevesurujballi@gmail.com>
2025-05-05 13:29:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const fetch = require("node-fetch");  // Ensure node-fetch is installed
+import express from "express";
+import fetch from "node-fetch";  // Ensure node-fetch is installed
 const app = express();
 const PORT = process.env.PORT || 5000;
 
